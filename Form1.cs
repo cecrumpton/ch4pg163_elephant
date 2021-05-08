274252ef528e6ce63f6848aa22536728b1b09fc4
Corey Crumpton <31524125+cecrumpton@users.noreply.github.com>
2021-05-07 21:24:35 -0500

    completed project

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch4pg163_elephant
+{
+    public partial class Form1 : Form
+    {
+        Elephant lloyd;
+        Elephant lucinda;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lloyd = new Elephant () { Name = "Lloyd",  EarSize = 40 };
+            lucinda = new Elephant () { Name = "Lucinda", EarSize = 33 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant swap = lloyd;
+            lloyd = lucinda;
+            lucinda = swap;
+            MessageBox.Show("Objects swapped", "Objects swapped");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            lloyd.TellMe("Hi", lucinda);
+            lloyd.SpeakTo(lucinda, "Hello");
+            lloyd = lucinda;
+            lloyd.EarSize = 4321;
+            lloyd.WhoAmI();
+        }
+    }
+}
